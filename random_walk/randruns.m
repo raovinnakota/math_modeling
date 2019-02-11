f14d6ab204084d6138e333db1c8e265f25ff642c
Math <math@06.alb1.sci.bard.edu>
2019-02-11 10:25:37 -0500

    Commenting randruns

--- random_walk/randruns.m
+++ random_walk/randruns.m
@@ -1,7 +1,12 @@
 function [ endpts ] = randruns(runs, steps, step_size)
+% Returns a vector of all the endpoints after running 
+% the 1 dimension random walk simulation *runs* times
 endpts = zeros(1, runs);
+% the return array initialized to the dimension of number of runs
 for i=1:runs
+    % iterate through, running the randomwalk simulation
     path = randwalk1dim(steps, step_size);
+    % add the final endpoint to the endpoints vector
     endpts(1,i) = path(1, steps);
 end
 end
