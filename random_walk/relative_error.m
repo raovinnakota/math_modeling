27103da771c9ebcdfb9a3c0a0a47d092314d643a
raovinnakota <vinnakotaeswar@gmail.com>
2019-02-18 11:40:26 -0500

    commenting relative_erro

--- random_walk/relative_error.m
+++ random_walk/relative_error.m
@@ -1,13 +1,22 @@
 function [rel_error] = relative_error(runs, steps, step_size)
+% Calculates the relative error using the error squared function on the
+% probabilities of the actual and the expected values
 endpts = randruns(runs, steps, step_size);
+% Get the end points for the function
 left = 0:steps;
 right = -steps:0;
 bins = left + right;
+% Create bins for the endpoints
 prob = expected_runs(steps, step_size);
 freqs = histcounts(endpts, bins);
+% Get the theoretical probabilities and the actual frequency using the
+% histcounts function
 act_prob = freqs ./ runs;
+% calculate probabilities by dividing frequencies by # of runs
 act_prob(1,steps+1) = 0;
+% Calculate the error
 rel_error = ((sum((act_prob-prob).^2))^.5);
+% Plot both
 plot(act_prob);
 hold on
 plot(prob)
