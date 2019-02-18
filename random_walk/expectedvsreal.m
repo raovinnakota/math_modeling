b2ccccc9ee6d4a4723fd111a590b181282b35e65
raovinnakota <vinnakotaeswar@gmail.com>
2019-02-18 10:58:06 -0500

    Finished calculating relative error for 1 dim random walk

--- random_walk/expectedvsreal.m
+++ random_walk/expectedvsreal.m
@@ -2,7 +2,7 @@ function [] = expectedvsreal (runs, steps, step_size)
 endpts = randruns(runs, steps, step_size);
 prob = expected_runs(steps, step_size);
 expected = prob .* runs;
-x = -steps/2:step_size:steps/2;
+x = -steps:step_size*2:steps;
 plot(x,expected);
 hold on
 histogram(endpts, 'FaceColor', 'none');
--- random_walk/relative_error.m
+++ random_walk/relative_error.m
@@ -1,4 +1,15 @@
 function [rel_error] = relative_error(runs, steps, step_size)
 endpts = randruns(runs, steps, step_size);
+left = 0:steps;
+right = -steps:0;
+bins = left + right;
 prob = expected_runs(steps, step_size);
+freqs = histcounts(endpts, bins);
+act_prob = freqs ./ runs;
+act_prob(1,steps+1) = 0;
+rel_error = ((sum((act_prob-prob).^2))^.5);
+plot(act_prob);
+hold on
+plot(prob)
+hold off
 end
