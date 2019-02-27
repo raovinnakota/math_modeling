0eef893a63e3bd8d677d6e392c797706de9b83a3
raovinnakota <vinnakotaeswar@gmail.com>
2019-02-27 10:14:56 -0500

    Commented plot_rel_error

--- random_walk/plot_rel_error.m
+++ random_walk/plot_rel_error.m
@@ -1,7 +1,9 @@
 function [ path ] = plot_rel_error(max_runs, steps, step_size)
+%plotting the relative error
 path = zeros(1, max_runs);
 i = 10;
 while i < max_runs
+    %Calculate the relative error and then plot it
     path(1,i) = relative_error(i, steps, step_size);
     i = i * 2   ;
 end
