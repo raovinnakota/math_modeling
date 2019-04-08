4df358a6bdcf2206a90ed9410911bdd833192acd
raovinnakota <vinnakotaeswar@gmail.com>
2019-04-08 10:46:29 -0400

    1D code

--- diffusion_convection/finite_diff.m
+++ diffusion_convection/finite_diff.m
@@ -1,4 +1,5 @@
 function [ path ] = finite_diff(tfinal, deltax, k, xfinal, tdelta)
+%1D FTCS diffusion/convection code
 [x,t] = meshgrid(0:deltax:xfinal, 0:tdelta:tfinal);
 alpha=(k*tdelta)/(deltax).^2;
 f=zeros(size(x));
@@ -9,11 +10,11 @@ for j=2:length(t(:,1))
     for i=2:length(x(1,:))-1
         f(j,i) = alpha*f(j-1,i-1)+((1-2*alpha)*f(j-1,i))+alpha*f(j-1,i+1);
     end
-    c=f;
-    pcolor(x,t,c)
+    path=f;
+    pcolor(x,t,path)
     n=length(x(1,:));
     y=zeros(2,17);
     y(2,:)=.5;
-    pcolor(x(1:2,:),y,[c(1,:);c(1,:)]);
+    pcolor(x(1:2,:),y,[path(1,:);path(1,:)]);
     shading interp
 end 
