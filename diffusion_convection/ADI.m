d3f811b986c644e0f8bc3e870fdefb4810b45e82
raovinnakota <vinnakotaeswar@gmail.com>
2019-04-10 10:29:53 -0400

    Adding ADI.m

--- diffusion_convection/ADI.m
+++ diffusion_convection/ADI.m
@@ -1,6 +1,6 @@
 function [ f ] = ADI(deltax, deltay, deltat, xfinal, yfinal, tfinal, k, u, v)
 %Diffusion/Convection in multiple dimensions
-%Worked with Elliott Goldstein on writing code
+%Worked with Elliott Goldstein and Kyle Zigner on writing code
 [x,y,t]=meshgrid(0:deltax:xfinal, 0:deltay:yfinal, 0:deltat:tfinal);
 
 cx=(u*deltat)/deltax;
@@ -9,15 +9,15 @@ cy=(v*deltat)/deltay;
 alphax=(k*deltat)/(deltax).^2;
 alphay=(k*deltat)/(deltay).^2;
 
-
-a=diag((1+alphax)*ones(m,1))+diag((-cx/4+(alphax/2))*(ones(m-1,1)),-1)+diag((cx/4-(alphax/2))*ones(m-1,1),1);
-b=diag((1-alphay)*ones(n,1))+diag((alphay/2+cy/4)*(ones(n-1,1)),-1)+diag((alphay/2-cy/4)*ones(n-1,1),1);
-
-m=length(x(1,:));
-n=length(y(1,:));
-p=length(t(1,:));
-p=length(t(1,:));
+m=length(x(:,1,1));
+n=length(y(1,:,1));
+p=length(t(1,1,:));
 fn=(zeros(m,n,p));
+
+ax=diag((1+alphax)*ones(m,1))+diag((-cx/4+(alphax/2))*(ones(m-1,1)),-1)+diag((cx/4-(alphax/2))*ones(m-1,1),1);
+ay=diag((1+alphay)*ones(m,1))+diag((-cy/4+(alphay/2))*(ones(m-1,1)),-1)+diag((cy/4-(alphay/2))*ones(m-1,1),1);
+bx=diag((1-alphax)*ones(n,1))+diag((alphax/2+cx/4)*(ones(n-1,1)),-1)+diag((alphay/2-cx/4)*ones(n-1,1),1);
+by=diag((1-alphay)*ones(n,1))+diag((alphay/2+cy/4)*(ones(n-1,1)),-1)+diag((alphay/2-cy/4)*ones(n-1,1),1);
 
 if1=ceil(1/3*length(fn(1,:)));
 if2=ceil(2/3*length(fn(1,:)));
@@ -26,18 +26,29 @@ if4=ceil(2/3*length(fn(:,1)));
 
 fn(if1:if2,if3:if4)=4;
 size(fn)
+%c=fn.*b;
+[px,lx,ux,signx]=splu(ax);
+[py,ly,uy,signy]=splu(ay');
+[pc,lc,uc,signc]=splu(bx);
+[pd,ld,ud,signd]=splu(by');
 
-c=fn.*b;
-[P,L,U,sign]=splu(a);
-f=zeros(m,n,p);
-[k l]=size(fn);
-t2=1:p;
-    for j=1:l
-       q=forward(L,P*c(:,j));
-       r=BackSub(U,q);
-       f(:,j,:)=r;
-    end
-    pcolor(f(:,:,1))
+[j k l] = size(fn);
+
+for i=1:p
+    test(:,:) = fn(i,:,:);
+    %It's clear that I have to perform step 2 inside the timesteps. Still
+    %working that out. 
+end
+
+%f=zeros(m,n,p);
+%[k l]=size(fn);
+%t2=1:p;
+%    for j=1:l
+%      q=forward(L,P*c(:,j));
+%       r=BackSub(U,q);
+%       f(:,j,:)=r;
+%    end
+%    pcolor(f(:,:,1))
     %%pcolor(f(:,:,1)
     %%shading 'interp
     %%pcolor([0 1], [1 2]
