4193d34eb13d00a8bf610555aa2f1b0fa2e9bd6e
raovinnakota <eswarv@sbcglobal.net>
2019-05-20 19:25:23 -0400

    Main is commented

--- final_project/main.m
+++ final_project/main.m
@@ -1,7 +1,16 @@
 function [out] = main(AB, SO, H, HR)
+%Main runs every single function. It takes the same input as pHit. 
+%First, creates hit_vec, the probability of getting a hit in the next at
+%bat
 hit_vec = pHit(AB, SO, H, HR);
+%Get the size of hit_vec so that we can create output of game with hit
+%probabilities
 [rows col] = size(hit_vec);
+%out = the vector of probabilities of getting a hit in the next game (after
+%simulations)
 out = zeros(rows, col);
+%Iterate through the hit_vec, using streak.m to find game probability and
+%then place that probability in out. 
 for i=1:rows
     out(i,1) = streak(hit_vec(i,1));
 end
